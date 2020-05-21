81121ac73fd33d56e1aa773022ab8c87229ea74c
Evgeny Artemov <53362802+evgeny_artemov@users.noreply.github.com>
2020-05-21 19:22:12 +0300

    stages section

--- src/main.js
+++ src/main.js
@@ -3,22 +3,47 @@ import '../node_modules/swiper/css/swiper.css';
 import '../node_modules/swiper/css/swiper.min.css';
 import Swiper from 'swiper';
 
-console.log('hello');
-console.log('hello2');
+//swiper plugin
 
 var mySwiper = new Swiper ('.swiper-container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+})
+
+//FAQ section questions list
+
+const faqQuestion = () => {
+  const questions = document.querySelectorAll(".questions__item");
+  const questionsContainer = document.querySelector(".faq__block--questions");
+
+  questionsContainer.addEventListener('click', ev => {
+    const activeQuest = ev.target.closest('.questions__item');
+
+    questions.forEach(el => {
+      el.children[0].classList.remove('active');
+      el.children[0].children[0].setAttribute("class","triangle--dis");
+      el.children[1].classList.remove('open');
+    })
+  
+    activeQuest.children[0].classList.add('active');
+    activeQuest.children[0].children[0].setAttribute("class","triangle--active");
+    activeQuest.children[1].classList.add('open');
+
   })
+
+}
+
+faqQuestion();
+
